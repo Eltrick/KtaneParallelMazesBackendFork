6f80b0343174de60f72a471375a428d5260fa3c0
Lucifer Voeltner <lucifervoeltner03@gmail.com>
2024-11-07 07:59:47 +0700

    Add all files

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,55 @@
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
- * - Run `npm run dev` in your terminal to start a development server
+ * - Run "npm run dev" in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
+ * - Run "npm run deploy" to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { SocketClient } from "./socket-client";
+
+declare global {
+  interface WebSocket {
+    accept(): void;
+  }
+
+  class WebSocketPair {
+    0: WebSocket;
+    1: WebSocket;
+  }
+
+  interface ResponseInit {
+    webSocket?: WebSocket;
+  }
+}
+
+const clients = {};
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request, env, ctx) {
+    return handleRequest(request);
+  },
+};
+
+async function handleRequest(request) {
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
+  }
+
+  const webSocketPair = new WebSocketPair();
+  const [client, server] = Object.values(webSocketPair);
+
+  server.addEventListener("open", (ws) => {
+    const _client = new SocketClient(client);
+    clients[_client.id] = _client;
+    _client.onDisconnect(() => delete clients[_client.id]);
+  })
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  });
+}
