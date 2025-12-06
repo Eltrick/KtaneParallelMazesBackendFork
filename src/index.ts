b3394088b49a15d079c267404c9e7fa2fb7d4962
Lucifer Voeltner <lucifer_gh@eltrick.uk>
2025-12-06 03:44:16 -0500

    Make things easier

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import { SocketClient } from "./socket-client";
 
 const clients: { [id: string]: SocketClient } = {};
 
-const wss = new WebSocketServer({ port: Number(process.env.PORT) || 8787 });
+const port_num = Number(process.env.PORT) || 3000;
+const wss = new WebSocketServer({ port: port_num });
+console.log("Listening on port " + port_num.toString());
+
 wss.on("connection", (ws: WebSocket) => {
 	const client = new SocketClient(ws);
 	clients[client.id] = client;
